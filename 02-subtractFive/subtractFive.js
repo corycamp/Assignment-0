8998c50cc94e2536f24027dbd054fb66bb018806
Cory Campbell <campbellcory2360@gmail.com>
2020-12-30 18:50:37 -0500

    First trial

--- 02-subtractFive/subtractFive.js
+++ 02-subtractFive/subtractFive.js
@@ -1,5 +1,8 @@
 function subtractFive(num) {
   // Insert code here;
+  var ans = num - 5;
+
+  return ans;
 }
 
 // Do not edit this line;
--- 03-cutInHalfAndFloor/cutInHalfAndFloor.js
+++ 03-cutInHalfAndFloor/cutInHalfAndFloor.js
@@ -1,5 +1,8 @@
 function cutInHalfAndFloor(num) {
   // Insert code here;
+  var ans = Math.floor(num/2);
+
+  return ans;
 }
 
 // Do not edit this line;
