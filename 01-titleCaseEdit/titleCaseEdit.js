a515e09aabef67bde8c984f78d21b3e1185390ea
Cory Campbell <campbellcory2360@gmail.com>
2020-12-30 17:40:12 -0500

    Second problem

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,17 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  var pieces = title.split(" ");
+  var whole = "";
+  for(var i = 0; i < pieces.length; i++)
+  {
+  	if(i < pieces.length -1)
+  	var word = pieces[i].charAt(0).toUpperCase() + pieces[i].slice(1, pieces[i].length + 1) + " ";
+  	else
+  	var word = pieces[i].charAt(0).toUpperCase() + pieces[i].slice(1, pieces[i].length + 1);
+    whole += word;
+  }
+  
+  return whole;
 }
 
 // Do not edit this line;
