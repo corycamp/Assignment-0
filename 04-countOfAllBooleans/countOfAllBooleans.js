813659015ed65dbce516320df3358083fbf55733
Cory Campbell <campbellcory2360@gmail.com>
2020-12-30 19:14:38 -0500

    Created package.json file for entire repository

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,14 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
+  var count = 0;
+  var total = 0;
+  while(count < arr.length){
+  	if(typeof(arr[count]) == "boolean"){
+  		total++;
+  	}
+  	count++;
+  }
+  return total;
 }
 
 // Do not edit this line;
--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,15 @@
 function countOfAllBooleansAndStrings(arr) {
   // Insert code here;
+  var total = 0;
+  var count = 0;
+
+  while(count < arr.length){
+  	if(typeof(arr[count]) == "boolean" || typeof(arr[count]) == "string"){
+  		total++;
+  	}
+  	count++;
+  }
+  return total;
 }
 
 // Do not edit this line;
